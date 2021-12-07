83243fbcbae22a8aae1ba63f14b6032ebd792080
Vartika <guptavartika624@gmail.com>
2021-12-07 22:31:51 +0530

    modify state increase qty

--- src/CartItem.js
+++ src/CartItem.js
@@ -12,6 +12,39 @@ class CartItem extends React.Component {
             qty: 1,
             img: ''
         }
+        //this.increaseQty = this.increaseQty.bind(this);
+    }
+    increaseQty = () => {
+        //this.state.qty += 1 
+        //console.log('this', this.state);
+
+        //setState() function to tell the react to change the state...it comes from 
+        //React.Component class.
+
+        //setState() form 1 (object form)
+        //if we don't require previous state, then use object form
+
+        //setState() function triggers re-rendering of our component with the updated value
+        // this.setState( 
+        //     //this obj gets shallow merged with the state obj changing only specified property
+        //     //and don't touch any other property
+
+        //     //after setting state react will automatically re-render our component with the updated
+        //     //data
+        
+        //     {
+        //         qty: this.state.qty+1
+        //     }
+        // );
+
+        //setState() form 2 (function form)
+        //If we require previous state then use the function form
+        this.setState((prevState)=>{
+            return {
+                qty: prevState.qty+1
+            }
+        });
+
     }
     render() {
         const {title, price, qty} = this.state;
@@ -27,8 +60,10 @@ class CartItem extends React.Component {
             <div style = {{color: 'grey'}}>Qty: {qty}</div>
             <div className = "cart-item-actions">
                 {/* cart-items */}
-                <img src="https://cdn-icons.flaticon.com/png/512/2740/premium/2740679.png?token=exp=1638886667~hmac=865f7e7800adabc3ba146acf462b83f5" alt="increase" className="action-icons"/>
-                <img src="https://cdn-icons.flaticon.com/png/512/3303/premium/3303893.png?token=exp=1638886795~hmac=5969a789d2dcadb14e556c224c5e1256" alt="decrease" className="action-icons"/>
+                
+                <img src="https://cdn-icons.flaticon.com/png/512/3303/premium/3303893.png?token=exp=1638886795~hmac=5969a789d2dcadb14e556c224c5e1256" alt="increase" className="action-icons" onClick={this.increaseQty}/>
+                <img src="https://cdn-icons.flaticon.com/png/512/2740/premium/2740679.png?token=exp=1638886667~hmac=865f7e7800adabc3ba146acf462b83f5" alt="decrease" className="action-icons"/>
+                
                 <img src="https://cdn-icons.flaticon.com/png/512/484/premium/484560.png?token=exp=1638886897~hmac=8aa28a55d26e67f55c15d571208cce8f" alt="delete" className="action-icons"/>
             </div>
             </div>
